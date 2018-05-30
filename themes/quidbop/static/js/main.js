b9e97c779aaf42a8a1c4b8c5325669491121dab3
zoltan <zoltan@zolesz-debian>
2018-05-30 17:48:47 +0300

    pp

--- themes/quidbop/static/js/main.js
+++ themes/quidbop/static/js/main.js
@@ -1,13 +1,12 @@
 var toggle = document.getElementsByClassName('nav-menu-toggle')[0];
 var menu = document.getElementsByClassName('nav-menu')[0];
-console.log(toggle);
 
 toggle.addEventListener('click', function() {
     menu.classList.toggle('shown');
     if (toggle.getAttribute('src') === '/img/menu.svg') {
-        toggle.setAttribute('src', '/img/close.svg')
+        toggle.setAttribute('src', '/img/close.svg');
     }
     else {
-        toggle.setAttribute('src', '/img/menu.svg')
+        toggle.setAttribute('src', '/img/menu.svg');
     }
 }, false);
