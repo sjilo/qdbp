6f10a465a0d5a42c4d8910058e4423a74930fd0f
zoltan <zoltan@zolesz-debian>
2018-05-30 22:00:39 +0300

    wtf

---
+++ public/js/main.js
@@ -0,0 +1,12 @@
+var toggle = document.getElementsByClassName('nav-menu-toggle')[0];
+var menu = document.getElementsByClassName('nav-menu')[0];
+
+toggle.addEventListener('click', function() {
+    menu.classList.toggle('shown');
+    if (toggle.getAttribute('src') === '/img/menu.svg') {
+        toggle.setAttribute('src', '/img/close.svg');
+    }
+    else {
+        toggle.setAttribute('src', '/img/menu.svg');
+    }
+}, false);
